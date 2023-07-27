678b15635702b739106302dd81c4a6e55540f94e
Ângelo Oliveira <angelooliveira@Angelos-MacBook-Pro.local>
2023-07-27 15:17:31 +0100

    Sorting DONE, ups language specific added

---
+++ Algorithms/Sorting/cppSpecific.cpp
@@ -0,0 +1,5 @@
+// For c++ arrays and vectors we can use the std::sort() function
+// Which use algorithms like quick sort, heap sort and insertion sort which is called introsort
+
+// For stuff like linked list we can use the std::list::sort() function
+// Which uses merge sort
